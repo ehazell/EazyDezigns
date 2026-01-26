f28910e25d11b6b8167e75a851e32c94943db832
Eazy Dezigns <eazydezigns@gmail.com>
2026-01-25 21:24:31 -0500

    changes to files

--- hed/js/filters.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('ruleApp.filters', []).filter('prettify', ['$escape', function($escape) {
-	return function(code) {
-		return prettyPrintOne($escape.xml(code));
-	};
-}]);
